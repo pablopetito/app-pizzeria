4ae96114f5c6810d6043771f6567f795c9324b07
pablopetito <pablo@petito.com.ar>
2018-04-07 21:28:20 -0300

    se agrego las tablas pedidos y pedidospizza

--- sql/db.sql
+++ sql/db.sql
@@ -16,11 +16,27 @@ create table clientes (
 );
 
 create table pedidos (
-  -- pablo
+	id int auto_increment primary key
+  ,	cliente int 
+  , fue_pedido_a_las datetime not null
+  , sera_entregado_a_las datetime not null
+  , estado varchar(30) not null
+  , foreign key (id) 
+	references clientes(nro_cliente)
+	on update cascade
+	on delete restrict
 );
 
 create table pedidos_pizzas (
-  -- pablo
+	pedido int
+  , pizza int
+  , cantidad int not null
+  , tamaño varchar[10] not null
+  , primary key (pedido, pizza)
+  , foreign key (pedido)
+	references pedidos(id)
+	on update cascade
+	on delete restrict
 );
 
 create table pizzas (
