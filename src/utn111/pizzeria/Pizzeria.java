1cc430bb8e0a087e380f3ab9cfd155cfd6e30b52
pablopetito <30782957+pablopetito@users.noreply.github.com>
2018-04-11 12:09:25 -0300

    Main Pizzeria  (#7)
    * borrador de Main Pizzeria
    
    * Main Pizzeria

--- src/utn111/pizzeria/Pizzeria.java
+++ src/utn111/pizzeria/Pizzeria.java
@@ -2,8 +2,46 @@ package utn111.pizzeria;
 
 public class Pizzeria {
 
-  public static void main(String[] args) {
-    // Pablo
-  }
+	public static void main(String[] args) throws Exception {
 
+		if (args.length != 1 || args == null) {
+			throw new IllegalArgumentException();
+		}
+
+		switch (args[0]) {
+
+		case "cajero":
+			cajero();
+			break;
+
+		case "admin":
+			admin();
+			break;
+
+		case "cocina":
+			cocina();
+			break;
+
+		default:
+			throw new IllegalArgumentException();
+		}
+
+	}
+
+	private static void cocina() {
+
+		// agregar codigo
+	}
+
+	private static void admin() {
+
+		// agregar codigo
+
+	}
+
+	private static void cajero() {
+
+		// agregar codigo
+
+	}
 }
