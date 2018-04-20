e3fa00eee720c3cf4604b04d26da98f1d2821d7d
pablopetito <pablo@petito.com.ar>
2018-04-20 09:39:15 -0300

    Se Elimino Class Boton

--- src/utn111/pizzeria/cocina/CocinaView.java
+++ src/utn111/pizzeria/cocina/CocinaView.java
@@ -7,19 +7,19 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class CocinaView extends JFrame {
 
-    public CocinaView() {
-    
-      this.setBounds(300,  50, 1000, 700);
-    
-      final JPanel panel = new JPanel();
-    
-      JButton botonActualizar = new JButton("Actualizar");
-      botonActualizar.setBounds(800, 600, 100, 30);
-      panel.add(botonActualizar);
-    
-      panel.setLayout(null);
+  public CocinaView() {
 
-      add(panel);
+    this.setBounds(300, 50, 1000, 700);
 
-    }
+    final JPanel panel = new JPanel();
+
+    JButton botonActualizar = new JButton("Actualizar");
+    botonActualizar.setBounds(800, 600, 100, 30);
+    panel.add(botonActualizar);
+
+    panel.setLayout(null);
+
+    add(panel);
+
+  }
 }
