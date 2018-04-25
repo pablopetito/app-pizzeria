302fdcdf5d06a776c23b27804c2f14e0f5ab62b8
Agus12s <agustinaprada@gmail.com>
2018-04-25 19:30:52 -0300

    Sacados los imports innecesarios

--- src/utn111/pizzeria/cajero/CajeroView.java
+++ src/utn111/pizzeria/cajero/CajeroView.java
@@ -1,9 +1,7 @@
 package utn111.pizzeria.cajero;
 
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class CajeroView extends JFrame {
