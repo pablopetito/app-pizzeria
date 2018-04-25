4188896c8f5e2d60ff0f0bc59d6ab87ed3bac25f
Agus12s <agustinaprada@gmail.com>
2018-04-25 15:29:41 -0300

    cambiados imports totales por individuales, agregada clase NuevoCliente view con el constructor y la estructura de Cajero view, que vuelve a estar vacío y recuperado el supress warning

--- src/utn111/pizzeria/cajero/CajeroView.java
+++ src/utn111/pizzeria/cajero/CajeroView.java
@@ -1,20 +1,10 @@
 package utn111.pizzeria.cajero;
 
-import javax.swing.*;
-import java.awt.*;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
 public class CajeroView extends JFrame {
-    public CajeroView() throws HeadlessException {
-        this.setBounds(300, 50, 1000, 700);
-
-        final JPanel panel = new JPanel();
-
-        JButton botonCancelar = new JButton("Cancelar pedido");
-        botonCancelar.setBounds(800, 600, 150, 30);
-        panel.add(botonCancelar);
-
-        panel.setLayout(null);
-
-        add(panel);
-    }
 }
