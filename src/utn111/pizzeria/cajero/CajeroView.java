919a25c4e1c08ef5e5150527ef14f3c57672ae75
Agus P Rios <30759994+Agus12s@users.noreply.github.com>
2018-05-13 16:36:15 -0300

    Agregados paneles principales y LayoutManagers

--- src/utn111/pizzeria/cajero/CajeroView.java
+++ src/utn111/pizzeria/cajero/CajeroView.java
@@ -1,22 +1,44 @@
 package utn111.pizzeria.cajero;
 
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 @SuppressWarnings("serial")
 public class CajeroView extends JFrame {
-    public CajeroView () {
-        final JPanel panel = new JPanel();
-        panel.setSize(500, 400);
-        setSize(panel.getSize());
-        panel.setLayout(null);
+  public CajeroView() {
+    final JPanel allPanelsContainer = new JPanel();
+    allPanelsContainer.setSize(500, 400);
+    setSize(allPanelsContainer.getSize());
+    allPanelsContainer.setLayout(new GridLayout(3, 1));// set LayoutManager
+    GridBagConstraints gbc = new GridBagConstraints();
+    Border eBorder = BorderFactory.createEtchedBorder();
     
-        JComboBox<?> dropClientes = new JComboBox<Object>();
-        dropClientes.setBounds(50, 30, 112, 20);
+    //UPPER CONTAINER
+    final JPanel upperContainer = new JPanel();
+    upperContainer.setBorder(BorderFactory.createTitledBorder(eBorder));
+    upperContainer.setLayout(new GridBagLayout());
+    //components
+    String comboBoxItems[] = {};
+    JComboBox dropClientes = new JComboBox<Object>(comboBoxItems);
+    dropClientes.setBorder(BorderFactory.createTitledBorder(eBorder));
+    gbc.fill = GridBagConstraints.WEST;
+    gbc.ipady = 40;
+    JButton boton = new JButton();
+    upperContainer.add(dropClientes,gbc);
+    upperContainer.add(boton);
+
+    //MIDDLE CONTAINER
+    final JPanel middleContainer = new JPanel();
+    middleContainer.setBorder(BorderFactory.createTitledBorder(eBorder));
+
+    //LOWER CONTAINER
+    final JPanel lowerContainer = new JPanel();
+    lowerContainer.setBorder(BorderFactory.createTitledBorder(eBorder));
     
-        panel.add(dropClientes);
-        
-        add(panel);
+    allPanelsContainer.add(upperContainer);
+    allPanelsContainer.add(middleContainer);
+    allPanelsContainer.add(lowerContainer);
+    add(allPanelsContainer);
   }
 }
