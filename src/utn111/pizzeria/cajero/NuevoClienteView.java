14aaa8efe0f6af7fce4ec09be6d06416f447f7b0
Andres Salomon <30812268+ZAMDO@users.noreply.github.com>
2018-05-19 11:17:03 -0300

    Update NuevoClienteView.java

--- src/utn111/pizzeria/cajero/NuevoClienteView.java
+++ src/utn111/pizzeria/cajero/NuevoClienteView.java
@@ -10,25 +10,25 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class NuevoClienteView extends JFrame {
 
-	public NuevoClienteView() {
+  public NuevoClienteView() {
 
-		setBounds(300, 50, 1000, 700);
+    setBounds(300, 50, 1000, 700);
 
-		final JPanel panel = new JPanel();
+    final JPanel panel = new JPanel();
 
-		JButton botonCancelar = new JButton("Cancelar");
-		botonCancelar.setBounds(800, 600, 150, 30);
-		botonCancelar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				NuevoClienteView.super.dispose();
-			}
-		});
+    JButton botonCancelar = new JButton("Cancelar");
+    botonCancelar.setBounds(800, 600, 150, 30);
+    botonCancelar.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        NuevoClienteView.super.dispose();
+      }
+    });
 
-		panel.add(botonCancelar);
+    panel.add(botonCancelar);
 
-		panel.setLayout(null);
+    panel.setLayout(null);
 
-		add(panel);
-	}
+    add(panel);
+  }
 
 }
