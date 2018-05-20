8a9da504fbdd376b7ab51ac14e5310bd845af792
pablopetito <pablo@petito.com.ar>
2018-05-20 18:30:26 -0300

    Creada clase Query - MODELO PROVISIORIO - PROBANDO

---
+++ src/bd/Query.java
@@ -0,0 +1,24 @@
+package bd;
+
+public class Query {
+
+  private final String sql;
+  private final Object[] params;
+
+  Query(String sql, Object[] params) {
+    this.sql = sql;
+    this.params = params;
+  }
+
+  public String getSql() {
+    return sql;
+  }
+
+  public Object[] getParams() {
+    return params;
+  }
+
+  public static SelectBuilder select(String from) {
+    return new SelectBuilder(from);
+  }
+}
