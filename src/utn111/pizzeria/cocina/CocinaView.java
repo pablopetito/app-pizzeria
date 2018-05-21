546dd3cf3bd00e06a032e31d6fb486379c4ea367
Alumno <alumno@labsistemas.dom>
2018-05-21 10:29:00 -0300

    Agregados correctamente los LM en CocinaView

--- src/utn111/pizzeria/cocina/CocinaView.java
+++ src/utn111/pizzeria/cocina/CocinaView.java
@@ -8,6 +8,7 @@ import javax.swing.JTable;
 import javax.swing.table.TableColumnModel;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 @SuppressWarnings("serial")
 public class CocinaView extends JFrame {
@@ -21,19 +22,43 @@ public class CocinaView extends JFrame {
     this.setBounds(300, 50, 1000, 700);
   }
 
+  /**
+   * Es la clase que crea toda la interfaz grafica.
+    */
   private void initPanelListadoPedidos() {
     final JPanel panel = new JPanel();
     panel.setLayout(new GridBagLayout());
-    GridBagConstraints gbc = new GridBagConstraints();
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.gridx = 0;
-    gbc.gridy = 0;
-    gbc.gridwidth = 3;
-    gbc.weightx = 1;
-    gbc.weighty = 1;
-    panel.add(crearTablaPedidos(), gbc);
-    panel.add(crearBotonActualizar());
+    panel.add(crearTablaPedidos(), configGbcTablaPedidos());
+    panel.add(crearBotonActualizar(), configGbcBotonActualizar());
     add(panel);
+  }
+
+  /**
+   * Clase configuradora del GridBagConstraints de tablaPedidos
+   * @return un GridBagConstraints configurado para la tabla
+   */
+  private GridBagConstraints configGbcTablaPedidos() {
+    GridBagConstraints gbcTablaPedidos = new GridBagConstraints();
+    gbcTablaPedidos.gridx = 0;
+    gbcTablaPedidos.gridy = 0;
+    gbcTablaPedidos.weightx = 10.0;
+    gbcTablaPedidos.weighty = 10.0;
+    gbcTablaPedidos.fill = GridBagConstraints.BOTH;
+    gbcTablaPedidos.insets = new Insets(10, 10, 10, 10);
+    return gbcTablaPedidos;
+  }
+
+  /**
+   * Clase configuradora del GridBagConstraints de botonActualizar
+   * @return un GridBagConstraints configurado para el boton
+   */
+  private GridBagConstraints configGbcBotonActualizar() {
+    GridBagConstraints gbcBotonActualizar = new GridBagConstraints();
+    gbcBotonActualizar.gridx = 0;
+    gbcBotonActualizar.gridy = 1;
+    gbcBotonActualizar.fill = GridBagConstraints.HORIZONTAL;
+    gbcBotonActualizar.insets = new Insets(0, 10, 10, 10);
+    return gbcBotonActualizar;
   }
 
   private static JButton crearBotonActualizar() {
@@ -53,5 +78,4 @@ public class CocinaView extends JFrame {
     scrollLista.setViewportView(tablaPedidos);
     return scrollLista;
   }
-
 }
