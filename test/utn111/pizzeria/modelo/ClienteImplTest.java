74061b271ca3eaf63cf2c9657cd423b272dc015b
Alumno <alumno@labsistemas.dom>
2018-05-23 10:47:51 -0300

    arrglado test de ClienteImpl

--- test/utn111/pizzeria/modelo/ClienteImplTest.java
+++ test/utn111/pizzeria/modelo/ClienteImplTest.java
@@ -6,12 +6,18 @@ import static org.junit.Assert.assertEquals;
 
 public class ClienteImplTest {
   @Test
-  public void clienteImplAlmacenaValoresCorrectos() {
+  public void clienteImplAlmacenaNombreCorrectamente() {
     ClienteDao clienteDao = new ClienteDao();
     clienteDao.setNombre("pepe");
+    ClienteImpl clienteImpl = new ClienteImpl(clienteDao);
+    assertEquals("pepe",clienteImpl.getNombre());
+  }
+
+  @Test
+  public void clienteImplAlmacenaNroClienteCorrectamente() {
+    ClienteDao clienteDao = new ClienteDao();
     clienteDao.setNroCliente(11);
     ClienteImpl clienteImpl = new ClienteImpl(clienteDao);
-    assertEquals(clienteDao.getNombre(),clienteImpl.getNombre());
-    assertEquals(clienteDao.getNroCliente(),clienteImpl.getNroCliente());
+    assertEquals(11,clienteImpl.getNroCliente());
   }
 }
