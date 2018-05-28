07acc9488c8796efea6331018f64e1c23e9038c3
Martin Scotta <martinscotta@gmail.com>
2018-05-27 21:36:12 -0300

    api execute

--- src/utn111/pizzeria/db/Conexion.java
+++ src/utn111/pizzeria/db/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
 
   private final Connection cnn;
 
-  private Conexion(Connection cnn) {
+  Conexion(Connection cnn) {
     this.cnn = cnn;
   }
 
@@ -24,8 +24,21 @@ public class Conexion {
     return new Consulta(cnn, sql);
   }
 
+  public Consulta preparar(Query query) {
+    return preparar(query.getSql())
+          .params(query.getParams());
+  }
+
   public int execute(String sql) {
     return preparar(sql).execute();
+  }
+
+  public int execute(Query query) {
+    return preparar(query).execute();
+  }
+
+  public int execute(QueryBuilder sql) {
+    return execute(sql.build());
   }
 
   /**
--- test/utn111/pizzeria/db/ConexionTest.java
+++ test/utn111/pizzeria/db/ConexionTest.java
@@ -1,19 +1,75 @@
 package utn111.pizzeria.db;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import org.junit.Test;
 import utn111.pizzeria.db.Conexion;
 
 public class ConexionTest {
-  
-  /**
-   * Pruebo una conexion con una BD en memoria.
-   */
-  @Test public void testUnaConexion() {
-    Connection cnn = Conexion.withSqlite();
-    assertNotNull(cnn);
+
+  private Conexion database() {
+    final Connection cnn = Conexion.withSqlite();
+
+    try {
+      cnn.prepareStatement("create table personas(id int, nombre varchar(200));")
+         .execute();
+
+      try (final PreparedStatement ps = cnn.prepareStatement("insert into personas values (?, ?)")) {
+
+        ps.setInt(1, 1);
+        ps.setString(2, "pepe");
+        ps.execute();
+
+        ps.setInt(1, 2);
+        ps.setString(2, "carlos");
+        ps.execute();
+
+        ps.setInt(1, 3);
+        ps.setString(2, "augusto");
+        ps.execute();
+      }
+    }
+    catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return new Conexion(cnn);
   }
+
+  @Test public void testExecuteString() {
+    final Conexion db = database();
+    final int deleted = db.execute("delete from personas");
+    assertEquals(3, deleted);
+  }
+
+  @Test public void testExecuteSimple() {
+    final Conexion db = database();
+    final int deleted = db.execute(
+      Query.delete("personas")
+    );
+    assertEquals(3, deleted);
+  }
+
+  @Test public void testExecuteUnString() {
+    final Conexion db = database();
+    final int deleted = db.execute(
+      Query.delete("personas")
+           .where("nombre = ?", "pepe")
+    );
+    assertEquals(1, deleted);
+  }
+
+  @Test public void testExecuteUnStringUnInt() {
+    final Conexion db = database();
+    final int deleted = db.execute(
+      Query.delete("personas")
+           .where("id = ? or nombre = ?", 2, "pepe")
+    );
+    assertEquals(2, deleted);
+  }
+
+
 }
